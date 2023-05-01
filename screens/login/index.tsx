d9e123e5dc7fdd3fabefa2e7eb72f0ef217ebc95
= <=>
2023-05-01 18:04:32 -0300

    feat: Adiciona a tela de cadastro de usuário no roteamento de 'login'

--- screens/login/index.tsx
+++ screens/login/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './Login';
+import CadastroUsuario from './CadastroUsuario';
 
 const Stack = createStackNavigator();
 
 function Index() {
   return (
     <Stack.Navigator initialRouteName="ScreenLogin" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="ScreenLogin" component={Login} />
+      <Stack.Screen name="ScreenLogin" component={Login} />
+      <Stack.Screen name="CadastroUsuario" component={CadastroUsuario} />
     </Stack.Navigator>
   );
 }
